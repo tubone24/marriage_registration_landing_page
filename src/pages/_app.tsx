f639cc38ffebda3fa6cdce09625feac341c16ef7
tubone24 <uu-ootsubo@kddi.com>
2021-05-21 02:11:44 +0900

    px

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,40 +8,39 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <ChakraProvider>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Hachi+Maru+Pop&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Nanum+Brush+Script&family=Nanum+Gothic&family=Nanum+Pen+Script&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Hachi+Maru+Pop&family=Nanum+Brush+Script&family=Nanum+Gothic&family=Nanum+Pen+Script&display=swap" rel="stylesheet" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <title>This is Next.js + TypeScript + Chakra + Jest + StoryBook + Recoil Template</title>
-          <meta property="og:title" content="This is Next.js + TypeScript + Chakra+Jest + StoryBook + Recoil Template" />
+          <title>GitHub婚姻届</title>
+          <meta property="og:title" content="GitHub婚姻届" />
           <meta
             property="og:description"
-            content="This template implements Next.js with TypeScript, uses ChakraUI for CSS, uses Jest for testing, StoryBook for cataloging components, and Recoil for state management by tubone24."
+            content="ちょっと口下手なあなた。気になる人に婚姻届を出しましょう！このギットハブのレポジトリをフォークしてコンフィグにあなたの情報を書いてプルリクするだけ。ギットハブアクションズが回って、あっという間に婚姻届ができちゃう。"
           />
-          <meta name="keywords" content="Template, Next.js, TypeScript, Chakra, Jest, StoryBook, Recoil" />
+          <meta name="keywords" content="婚姻届, GitHubActions" />
           <meta property="og:type" content="website" />
           <meta
             property="og:url"
-            content="https://my-nextjs-template-tubone24.vercel.app/"
+            content="https://marriage-registration-landing-page.vercel.app/"
           />
           <meta property="og:image" content="https://i.imgur.com/M2PhISt.png" />
-          <meta property="og:site_name" content="This is Next.js + TypeScript + Chakra+Jest + StoryBook + Recoil Template" />
+          <meta property="og:site_name" content="GitHub婚姻届" />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@meitante1conan" />
           <meta
             name="twitter:url"
-            content="https://my-nextjs-template-tubone24.vercel.app/"
+            content="https://marriage-registration-landing-page.vercel.app/"
           />
-          <meta name="twitter:title" content="This is Next.js + TypeScript + Chakra+Jest + StoryBook + Recoil Template" />
+          <meta name="twitter:title" content="GitHub婚姻届" />
           <meta
             name="twitter:description"
             content="This template implements Next.js with TypeScript, uses ChakraUI for CSS, uses Jest for testing, StoryBook for cataloging components, and Recoil for state management by tubone24."
           />
           <meta name="twitter:image" content="https://i.imgur.com/M2PhISt.png" />
           <meta property="fb:app_id" content="280941406476272" />
-          <link rel="canonical" href="https://my-nextjs-template-tubone24.vercel.app/" />
+          <link rel="canonical" href="ttps://marriage-registration-landing-page.vercel.app/" />
           <link rel="apple-touch-icon" href={'https://i.imgur.com/M2PhISt.png'} />
           <link rel="icon" href="/favicon.ico" />
         </Head>
