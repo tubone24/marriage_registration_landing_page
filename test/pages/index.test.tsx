daac5319279edd5590ac8abcbfcfcef0da9af3ec
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2021-11-08 09:57:14 +0900

    Update index.test.tsx

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { render } from 'test/testUtils'
 import HomePage from 'src/pages'
-import { RecoilRoot } from 'recoil'
 
 global.fetch = require('node-fetch');
 
 test('index snapshot', () => {
   const { container } = render(
-    <RecoilRoot>
-      <HomePage />
-    </RecoilRoot>
+    <HomePage />
     )
   expect(container).toMatchSnapshot()
 })
